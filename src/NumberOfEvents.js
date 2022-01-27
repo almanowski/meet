37a093397a0dd715b3f406cfa7f2fc39a3a4aebe
almanowski <anna.l.manowski@gmail.com>
2022-01-27 11:47:42 +0100

    Add alerts

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 
+import {ErrorAlert} from './Alert';
+
 class NumberOfEvents extends Component {
   constructor(props) {
     super(props);
@@ -12,15 +14,18 @@ class NumberOfEvents extends Component {
     let value = event.target.value;
     if (value <= 0) {
       this.setState({
-        eventNumber: 0
+        eventNumber: 0,
+        errorText: 'Please select a number higher than 0',
       });
     } else if (value > 32) {
       this.setState({
-        eventNumber: 32
+        eventNumber: 32,
+        errorText: 'Please select a number lower than 33',
       });
     } else {
       this.setState({
         eventNumber: value,
+        errorText: ''
       });
     }
     if (this.props.updateNumberOfEvents)
@@ -31,6 +36,7 @@ class NumberOfEvents extends Component {
   render() {
     return (
       <div className="number-of-events">
+        <ErrorAlert text={this.state.errorText} />
         <p>Number of Events:</p>
         <input type="number" className="number" value={this.state.eventNumber} 
         onChange={this.handleInputChanged} 
